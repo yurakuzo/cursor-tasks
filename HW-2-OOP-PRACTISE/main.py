7132f02a98860c7ba464142ebd740bacb4469604
yurakuzo <yurakuzo20@gmail.com>
2023-03-16 23:57:48 +0200

    Fixed HW-2-OOP-PRACTISE

--- HW-2-OOP-PRACTISE/main.py
+++ HW-2-OOP-PRACTISE/main.py
@@ -4,12 +4,9 @@ class Person:
         self.age = age
         self.money = money
         self.houses = houses
-
-    def __str__(self) -> str:
-        return self.info()
     
     def info(self):
-        print(f"{self.name} is {self.age}y.o.\nearns: ${self.money}\nowns: {self.houses}")
+        print(f"{self.name} is {self.age}y.o.\nearns: ${self.money}\nowns: {x if (x := self.houses) else 'no property'}")
 
     def make_money(self, amount):
         self.money += amount
@@ -25,9 +22,23 @@ class House:
         self.area = area
         self.cost = cost
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return f"{self.area}m2 house for ${self.cost}"
 
     def apply_discount(self, discount=.1):
         self.cost *= 1 - discount
+
+small_typical_house = House(40, 40_000)
+villa = House(350, 450_000)
+penthouse = House(150, 150_000)
+
+default_person = Person('Jake', 27, 100_000, [small_typical_house])
+default_person.info()
+
+rich_person = Person('John', 45, 35_000_000, [villa, penthouse])
+rich_person.info()
+
+poor_person = Person('Mike', 17, 50)
+poor_person.info()
+
 
